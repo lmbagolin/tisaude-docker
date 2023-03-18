2d39df5d7391d6fd57971181a303a1a4fd2154eb
Leonel <lmbagolin@gmail.com>
2023-03-18 15:51:33 -0300

    INIT

---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+set -e
+
+LARADOCK_DIR=`pwd`
+API_DIR=$(cd ../tisaude-api && pwd)
+
+echo "Diretorios:"
+echo "LARADOCK_DIR: ${LARADOCK_DIR}"
+echo "API_DIR: ${API_DIR}"
+
+echo ""
+echo "Evironments: "
+cp  ${LARADOCK_DIR}/.env.example ${LARADOCK_DIR}/.env
+cp  ${API_DIR}/.env.example ${API_DIR}/.env
+echo ""
+
+echo "Criando Containers"
+docker-compose up -d nginx mysql
+echo ""
+
+echo "Registrando WORKSPACE"
+WORKSPACE_NAME=`docker ps --format "{{.Names}}" --filter ancestor=tisaude_workspace`
+WORKSPACE_MYSQL=`docker ps --format "{{.Names}}" --filter ancestor=tisaude_mysql`
+
+echo "CRIANDO BD conselhos"
+docker exec -i ${WORKSPACE_MYSQL} /bin/sh -c 'mysql -u root -proot < ./docker-entrypoint-initdb.d/createdb.sql'
+
+echo "Executando configurações e migrations da API"
+docker exec -i -w /var/www/tisaude-api ${WORKSPACE_NAME} composer install
+docker exec -i -w /var/www/tisaude-api ${WORKSPACE_NAME} php artisan migrate:fresh --seed
+docker exec -i -w /var/www/tisaude-api ${WORKSPACE_NAME} php artisan storage:link
+docker exec -i -w /var/www/tisaude-api ${WORKSPACE_NAME} php artisan jwt:secret
+
+echo ""
+echo "Dev setup done!"
---
+++ dockerUp.sh
@@ -0,0 +1,5 @@
+docker-compose down
+
+echo "Criando Containers"
+docker-compose up -d nginx mysql
+echo ""
